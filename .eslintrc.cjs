a67a076152664d8d432b6e3f48e95483f976cfdd
diwoni <70441308+Diwoni@users.noreply.github.com>
2025-11-27 17:06:51 +0100

    test: husky 테스트

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: true,
   },
   plugins: [
     '@typescript-eslint',
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import { createRoot } from 'react-dom/client';
-import App from '@/App';
 import '@/styles/index.css';
+
+import { createRoot } from 'react-dom/client';
+
+import App from '@/App';
 
 const root = createRoot(document.getElementById('root')!);
 
+console.log('husky test');
 root.render(<App />);
